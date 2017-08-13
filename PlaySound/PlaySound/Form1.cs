5597d3a73920abaef1f5964946d12de4401840bb
Ananth Racherla <dev@ananthr.com>
2017-08-12 18:30:38 -0700

    Demonstrates how to loop an audio file

---
+++ PlaySound/PlaySound/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace PlaySound
+{
+	public partial class Form1 : Form
+	{
+		public SoundPlayer SoundPlayer { get; } = new SoundPlayer("alarm.wav");
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			SoundPlayer.PlayLooping();
+		}
+
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			SoundPlayer.Stop();
+		}
+	}
+}
